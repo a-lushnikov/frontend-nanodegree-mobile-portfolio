59862bf7497002c2a5b5d1fba17f205e38b8d39a
Anatoliy Lushnikov <toliantm@narod.ru>
2015-06-11 02:52:25 +0200

    Added correct gulp tasks and optimized images

--- README.md
+++ README.md
@@ -1,7 +1,26 @@
-## Grunt task
-run
+## Performance optimization project
+
+This project was completed as a part of udacity frontend developer nanodegree programm. The idea was to optimize a given web-site to achieve adequate frame-rate for several terminally slow pages. Most of the exercise took place in main.js which is some weird JS with effects for web-site with lots of pizzas flying all over the screen.
+
+# Building project
+Before you will be able to run this project you need to build it. Build is done with gulp. Run
 
 npm install
-grunt
+gulp clean
+gulp min
 
-to get minified JS
+to get minified JS and CSS. That is necessary to run web site.
+
+# Server setup
+Chrome page speed insights was showing issues related to caching, encoding and charset. This might only be fixed on the server level. For me I've been using IIS. What you need to do is to set some HTTP headers in response.
+
+Those should be:
+ Cache-Control : max-age=86400
+ Content-Type : text/html; charset=UTF-8
+ Vary : Accept-Encoding
+
+Also set content expiry date. Sample configuration for IIS provided (see web.config)
+
+
+
+When setup is done - run your server and access index.html
--- gulpfile.js
+++ gulpfile.js
@@ -10,17 +10,20 @@ gulp.task('clean', function () {
     del('build');
     del('views/css/*.min.css');
     del('css/*.min.css');
+    del('views/js/*.min.js');
+    del('js/*.min.js');
+    del('img/*o3*');
 })
 
 // copies src to deploy folder
-gulp.task('deploy', function() {
-    return gulp.src(['./**/*', '!node_modules', '!node_modules/**/*'])
-        .pipe(gulp.dest('build'))
-        .on('end', function () {
-            del('build/gulpfile.js');
-            del('build/package.json');
-        });
-});
+// gulp.task('deploy', function() {
+//     return gulp.src(['./**/*', '!node_modules', '!node_modules/**/*'])
+//         .pipe(gulp.dest('build'))
+//         .on('end', function () {
+//             del('build/gulpfile.js');
+//             del('build/package.json');
+//         });
+// });
 
 gulp.task('min-css1', function() {
   return gulp.src('css/*.css')
@@ -37,3 +40,32 @@ gulp.task('min-css2', function() {
 });
 
 gulp.task('min-css', ['min-css1', 'min-css2']);
+
+gulp.task('min-js1', function() {
+  return gulp.src('js/*.js')
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('js/'));
+});
+
+gulp.task('min-js2', function() {
+  return gulp.src('views/js/*.js')
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('views/js/'));
+});
+
+gulp.task('min-js', ['min-js1', 'min-js2']);
+
+
+// adopted from https://markgoodyear.com/2014/01/getting-started-with-gulp/
+// use this to get optmized images
+gulp.task('img', function() {
+  return gulp.src('img/**/*')
+    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+    .pipe(rename({suffix: 'o3'}))
+    .pipe(gulp.dest('img'))
+});
+
+gulp.task('min', ['min-css', 'min-js', 'img']);
+
